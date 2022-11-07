3823b99291d7c86966631ff06f891b1dff5f7b62
Vinicius Benicio de Santana <vinicius.santana@talkcomm.com.br>
2022-11-07 17:32:53 -0300

    Feat: Props React e Componetizando o formulário
     * Utilizando Props no React
     * Componetizando a aplicação de Formulário

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import {useState} from 'react';
 
 import './styles/App.css';
-import './styles/PostForm.css';
-import './styles/Feed.css';
 
-import userIcon from './images/user.svg';
-import paperPlaneIcon from './images/paper-plane.svg';
-import clockItem from './images/clock.svg';
-import emptyFolderIcon from './images/empty-folder.svg';
+
+import Feed from './components/Feed';
+import PostForm from './components/PostForm';
 
 export default function App(){
-    const [history, setHistory] = useState('');
-    const [userName, setUserName] = useState('');
     const [posts, setPosts] = useState([]);
 
-function handleSubmit(event){
-   event.preventDefault();
-   
+function handleSubmit({history, userName}){
+
    setPosts([
     ...posts,
     {
@@ -26,72 +20,13 @@ function handleSubmit(event){
         publishedAt: new Date(),
     }
     ]);
-
-    setHistory('');
-    setUserName('');
-}
+  }
 
     return (
         <div className="wrapper">
-         <form className="post-form" onSubmit={handleSubmit}>
-            <input 
-            value={history}
-            placeholder="Escreva uma nova historia..."
-            onChange={(event) => setHistory(event.target.value)}
-            />
-
-            <div>
-                <img src={userIcon} alt="User" />
-                <input 
-                value={userName}
-                placeholder="Digite seu nome..."
-                onChange={(event) => setUserName(event.target.value)}
-                />
-
-                <button type="submit">
-                    <img src={paperPlaneIcon} alt="Paper Planne" />
-                    Publicar
-                </button>
-            </div>
-         </form>
+          <PostForm onSubmit={handleSubmit}/>
          <main>
-            {posts.length === 0 && (
-             <div className="feed-status">
-              <img src={emptyFolderIcon} alt="Empty folder"/>
-                <h1>Não encontramos nada</h1>
-                <h2>
-                Parece que você e seus amigos não postaram nada, 
-                Comece a escrever uma nova história!
-                </h2>
-                </div>
-            )}
-            {posts.length > 0  && 
-            (
-            <>
-            <header>
-                <h1>Seu Feed</h1>
-                <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
-            </header>
-
-            <section className="feed">
-            {posts.map((posts) => (
-                <article key={posts.id}>
-                <p>{posts.content}</p>
-                <footer>
-                    <div className="user-details">
-                        <img src={userIcon} alt="User" />
-                        <strong>{posts.userName}</strong>
-                    </div>
-                    <div className="time">
-                        <img src={clockItem} alt="Clock" />
-                        <span>{posts.publishedAt.toLocaleDateString('pt-br')}</span>
-                    </div>
-                </footer>
-            </article>
-            ))}
-            </section>
-            </>
-            )}
+            <Feed posts={posts}/>
          </main>
         </div>
     );
---
+++ src/components/Feed.js
@@ -0,0 +1,50 @@
+import userIcon from '../images/user.svg';
+import clockItem from '../images/clock.svg';
+import emptyFolderIcon from '../images/empty-folder.svg';
+
+
+
+import '../styles/Feed.css';
+
+
+export default function Feed(props){
+    if(props.posts.length === 0){
+        return (
+            <div className="feed-status">
+              <img src={emptyFolderIcon} alt="Empty folder"/>
+                <h1>Não encontramos nada</h1>
+                <h2>
+                Parece que você e seus amigos não postaram nada, 
+                Comece a escrever uma nova história!
+                </h2>
+                </div>
+        );
+    }
+
+    return (
+        <>
+            <header>
+                <h1>Seu Feed</h1>
+                <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
+            </header>
+
+            <section className="feed">
+            {props.posts.map((posts) => (
+                <article key={posts.id}>
+                <p>{posts.content}</p>
+                <footer>
+                    <div className="user-details">
+                        <img src={userIcon} alt="User" />
+                        <strong>{posts.userName}</strong>
+                    </div>
+                    <div className="time">
+                        <img src={clockItem} alt="Clock" />
+                        <span>{posts.publishedAt.toLocaleDateString('pt-br')}</span>
+                    </div>
+                </footer>
+            </article>
+            ))}
+            </section>
+            </>
+    );
+}
